2c852235b616b56902a8ae8189f1a86969401686
Samuel <samuelrey010@gmail.com>
2021-05-25 17:14:20 -0700

    Implement Next command
    When the user at the head of the queue wants to skip their turn, they
    use this command to push them back to the tail of the queue.

---
+++ cmd/next.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"../framework"
+)
+
+func Next(ctx *framework.Context) {
+	if len(*ctx.EnrolledUsers) <= 1 {
+		return
+	}
+
+	skipUserID := (*ctx.EnrolledUsers)[0]
+	if ctx.User.ID != skipUserID {
+		return
+	}
+
+	(*ctx.EnrolledUsers) = append((*ctx.EnrolledUsers)[1:], skipUserID)
+	content := fmt.Sprintf("%v, see you next time around!", ctx.User.Username)
+	ctx.Reply(content)
+
+	nextUserID := (*ctx.EnrolledUsers)[0]
+	nextUser, err := ctx.Discord.User(nextUserID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	content = fmt.Sprintf("%v, you're up next!", nextUser.Username)
+	ctx.Reply(content)
+}
