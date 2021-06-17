3cb87137c833d55558017f73e392c33910266601
Samuel <samuelrey010@gmail.com>
2021-06-16 21:27:50 -0700

    Remove unused Spotify logic

--- cmd/create.go
+++ cmd/create.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/samuelrey/spot-discord/framework"
-	"github.com/samuelrey/spot-discord/spotify"
 )
 
 const StrPlaylistCreatedFmt = "Done! :tada: Now it's up to to you to " +
@@ -21,26 +20,7 @@ func Create(ctx *framework.Context) {
 		return
 	}
 
-	spotifyClient, err := spotify.NewClient(ctx.Actor.ID, func(url string) {
-		ctx.DirectMessage(ctx.Actor.ID, url)
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	id, err := spotifyClient.CurrentUserID()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	playlist, err := spotifyClient.CreatePlaylist(*id, "einstock")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	content := fmt.Sprintf(StrPlaylistCreatedFmt, playlist.URL)
+	// TODO create playlist.
+	content := fmt.Sprintf(StrPlaylistCreatedFmt, "URL")
 	ctx.DirectMessage(ctx.Actor.ID, content)
 }
