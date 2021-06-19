938818f936ed86105c7abf9c7795db4b94305925
Samuel <samuelrey010@gmail.com>
2021-06-19 10:35:39 -0700

    Move CommandContext to clarify relationship
    As Context in a separate file, it wasn't entirely clear how it was that
    this struct was related to the rest of the application. The rename and
    move into framework/command demonstrates that these are dependencies
    required by Commands.

--- framework/command.go
+++ framework/command.go
@@ -1,10 +1,10 @@
 package framework
 
 type (
-	Command        func(*Context)
+	Command        func(*CommandContext)
 	CmdMap         map[string]Command
 	CommandHandler struct {
-		cmds CmdMap
+		CmdMap
 	}
 )
 
@@ -13,10 +13,17 @@ func NewCommandHandler() *CommandHandler {
 }
 
 func (cmdHandler CommandHandler) Get(name string) (*Command, bool) {
-	cmd, found := cmdHandler.cmds[name]
+	cmd, found := cmdHandler.CmdMap[name]
 	return &cmd, found
 }
 
 func (cmdHandler CommandHandler) Register(name string, command Command) {
-	cmdHandler.cmds[name] = command
+	cmdHandler.CmdMap[name] = command
+}
+
+type CommandContext struct {
+	Messager
+	EnrolledUsers *[]User
+	Actor         User
+	ChannelID     string
 }
