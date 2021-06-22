6d799acadecf36298a0915bbf90cc57dfd2b9abe
Samuel Rey <samuelrey010@gmail.com>
2021-06-21 22:54:51 -0700

    Remove redundant DiscordBuilder
    As an abstraction, it was unclear what DiscordBuilder was responsible
    for. The key responsibility it owned was the discordgo.Session.
    
    Move to using discordgo directly in main.
    
    Another side effect of this was the need to make the dependencies
    globally scoped in order to access from the body of the handleMessage
    function. I wonder if there is a stronger way to clear that up.

--- main.go
+++ main.go
@@ -5,34 +5,48 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/samuelrey/spot-discord/cmd"
 	"github.com/samuelrey/spot-discord/discord"
 	"github.com/samuelrey/spot-discord/framework"
 	"github.com/samuelrey/spot-discord/spotify"
 )
 
-func main() {
-	enrolledUsers := make([]framework.MessageUser, 0)
+const prefix = "!"
 
-	cmdHandler := framework.NewCommandHandler()
-	registerCommands(*cmdHandler)
+var (
+	ch            *framework.CommandHandler
+	pc            framework.PlaylistCreator
+	enrolledUsers []framework.MessageUser
+	err           error
+)
+
+func main() {
+	enrolledUsers = make([]framework.MessageUser, 0)
+
+	ch = framework.NewCommandHandler()
+	registerCommands(*ch)
 
 	spotifyConfig := spotify.LoadConfig("secrets_spotify.json")
 	sa := spotify.NewSpotifyAuthorizer(spotifyConfig)
-	sp, err := sa.AuthorizeUser()
+	pc, err = sa.AuthorizeUser()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
 	discordConfig := discord.LoadConfig("secrets_discord.json")
-	d, err := discord.NewDiscordBuilder(discordConfig, cmdHandler, &enrolledUsers, sp)
+	d, err := discordgo.New("Bot " + discordConfig.Token)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	d.AddHandler(handleMessage)
+	d.Identify.Intents = discordgo.IntentsGuildMessages
 
 	log.Println("Discord session opening.")
 	err = d.Open()
@@ -63,4 +77,45 @@ func registerCommands(cmdHandler framework.CommandHandler) {
 	cmdHandler.Register("list", cmd.List)
 	cmdHandler.Register("next", cmd.Next)
 	cmdHandler.Register("create", cmd.Create)
+}
+
+func handleMessage(dg *discordgo.Session, message *discordgo.MessageCreate) {
+	user := message.Author
+	if user.Bot {
+		return
+	}
+
+	content := message.Content
+
+	if len(content) <= len(prefix) {
+		return
+	}
+
+	if content[:len(prefix)] != prefix {
+		return
+	}
+
+	args := strings.Fields(content[len(prefix):])
+	name := strings.ToLower(args[0])
+
+	command, found := ch.Get(name)
+	if !found {
+		return
+	}
+
+	ctx := framework.CommandContext{
+		Messager: &discord.DiscordMessager{
+			Session:   dg,
+			ChannelID: message.ChannelID,
+		},
+		PlaylistCreator: pc,
+		PlaylistName:    "Einstok",
+		EnrolledUsers:   &enrolledUsers,
+		Actor: framework.MessageUser{
+			ID:       user.ID,
+			Username: user.Username,
+		},
+	}
+	c := *command
+	c(&ctx)
 }
