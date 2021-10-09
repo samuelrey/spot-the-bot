84021d6c0facd7670ebc2b26908b809d5f744ba2
Samuel <samuelrey010@gmail.com>
2021-10-08 22:15:56 -0700

    Test Rotation Rotate

--- rotation/rotation_test.go
+++ rotation/rotation_test.go
@@ -9,45 +9,6 @@ import (
 
 type RotationSuite struct{ suite.Suite }
 
-func (suite RotationSuite) TestHeadPop() {
-	q := Rotation{}
-
-	// empty queue
-	actual := q.Current()
-	suite.Require().Nil(actual)
-
-	actual = q.pop()
-	suite.Require().Nil(actual)
-
-	// nonempty queue
-	au := message.User{ID: "amethyst#4422", Username: "amethyst"}
-	ou := message.User{ID: "osh#1219", Username: "osh"}
-	q = NewRotation([]message.User{au, ou})
-
-	actual = q.Current()
-	suite.Require().Equal(&au, actual)
-
-	actual = q.pop()
-	suite.Require().Equal(&au, actual)
-	suite.Require().Equal(1, q.Length())
-
-	actual = q.Current()
-	suite.Require().Equal(&ou, actual)
-}
-
-func (suite RotationSuite) TestPushRemove() {
-	q := Rotation{}
-	mu := message.User{ID: "amethyst#4422", Username: "amethyst"}
-
-	q.push(mu)
-	actual := q.Current()
-	suite.Require().Equal(&mu, actual)
-
-	q.Leave(mu)
-	actual = q.Current()
-	suite.Require().Nil(actual)
-}
-
 func (suite RotationSuite) TestJoin() {
 	q := Rotation{}
 	mu := message.User{ID: "amethyst#4422", Username: "amethyst"}
@@ -63,6 +24,25 @@ func (suite RotationSuite) TestJoin() {
 	suite.Require().NotNil(err)
 	suite.Require().Equal(1, q.Length())
 	suite.Require().True(q.contains(mu))
+}
+
+func (suite RotationSuite) TestRotate() {
+	q := Rotation{}
+	m1 := message.User{ID: "amethyst#4422", Username: "amethyst"}
+	m2 := message.User{ID: "osh#1219", Username: "osh"}
+
+	user, err := q.Rotate()
+
+	suite.Require().Nil(user)
+	suite.Require().NotNil(err)
+
+	_ = q.Join(m1)
+	_ = q.Join(m2)
+	user, err = q.Rotate()
+
+	suite.Require().Nil(err)
+	suite.Require().NotNil(user)
+	suite.Require().Equal(m2, *user)
 }
 
 func (suite RotationSuite) TestLeave() {
